2503ef0185759d97735e1b676996cc7f54464e43
christianestlund <christian.estlund@wsu.edu>
2024-02-17 21:52:44 -0800

    database.py masterlist

--- database.py
+++ database.py
@@ -1,22 +1,45 @@
 import redis
 
 #setting up redis db values (currently localhost because cloud service doesn't want to be nice)
-redis_host = "localhost"
-redis_port = 15558  #port for cloud database (in theory)
-#redis_password = "8p4QwHv9yZ5FmRxDelkKm26HSRSc1pQZ" #default user password
-redis_db = 0  # Default Redis database index
+redisHost = "localhost"
+redisPort = 15558  #port for cloud database (in theory)
+#redisPassword = "8p4QwHv9yZ5FmRxDelkKm26HSRSc1pQZ" #default user password
+redisDb = 0  # Default Redis database index
 
-# Create a connection to the Redis Cloud database 
+# Create a connection to the Redis Cloud database #
 
-redis_connection = redis.StrictRedis(
-    host=redis_host,
-    port=redis_port,
-    #password=redis_password,
-    db=redis_db,
+redisConnection = redis.StrictRedis(
+    host=redisHost,
+    port=redisPort,
+    #password=redisPassword,
+    db=redisDb,
     decode_responses=True,  # Decode responses from bytes to strings
     socket_timeout=5
 )
 
-test_list1 = redis_connection.lpush("contained in list 1")
-test_list2 = redis_connection.lpush(test_list1)
-print(test_list2)
+######
+
+masterList = "masterList"
+
+def addListRef(listName):
+    redisConnection.rpush(masterList, listName)
+
+def getListRefs():
+    return redisConnection.lrange(masterList, 0, -1)
+
+list1R = "list1"
+list2R = "list2"
+list3R = "list3"
+
+redisConnection.rpush(list1R, "1", "2", "3")
+redisConnection.rpush(list2R, "a", "b", "c")
+
+addListRef(list1R)
+addListRef(list2R)
+
+references = getListRefs()
+
+for reference in references:
+    items = redisConnection.lrange(reference, 0, -1)
+    print(items)
+
