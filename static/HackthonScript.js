8178abfa8740de4d85d95915ae26b6ee167f1b93
Zachary Adelson <zachary.adelson@wsu.edu>
2024-02-18 02:27:35 -0800

    uhhhh

--- static/HackthonScript.js
+++ static/HackthonScript.js
@@ -1,20 +1,21 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+// Retrieve saved list items from localStorage on page load
+document.addEventListener('DOMContentLoaded', function() {
+  var savedList = localStorage.getItem('myToDoList');
+  if (savedList) {
+    document.getElementById('myUL').innerHTML = savedList;
+    addCloseButtonListeners(); // Re-attach event listeners to close buttons
+  }
+});
 
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+function addCloseButtonListeners() {
+  var closeButtons = document.getElementsByClassName("close");
+  for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+      saveList(); // Save the list after item is removed
+    };
   }
 }
 
@@ -23,8 +24,15 @@ var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
+    saveList(); // Save the list after item is checked/unchecked
   }
 }, false);
+
+// Save the list to localStorage
+function saveList() {
+  var listHTML = document.getElementById('myUL').innerHTML;
+  localStorage.setItem('myToDoList', listHTML);
+}
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
@@ -45,10 +53,6 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+  addCloseButtonListeners(); // Attach event listener to the new close button
+  saveList(); // Save the updated list
 }
