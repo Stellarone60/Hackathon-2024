f1e704ed6a11aad4340d4f0f9ed8b09f4120942c
Zachary Adelson <zachary.adelson@wsu.edu>
2024-02-17 17:45:08 -0800

    HEELPPP

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ oauth.register(
 @app.route("/")
 def home():
     return render_template(
-        "home.html",
+        "HackthonMainPage.html",
         session=session.get("user"),
         pretty=json.dumps(session.get("user"), indent=4),
     )
@@ -52,10 +52,6 @@ def login():
     return oauth.auth0.authorize_redirect(
         redirect_uri=url_for("callback", _external=True)
     )
-
-@app.route("/HackthonMainPage.html")
-def mainpage():
-    return(render_template("HackthonMainPage.html"))
 
 @app.route("/logout")
 def logout():
